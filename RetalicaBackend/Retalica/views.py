628280060b4f444df6a9d59dc969a042c00e9f17
Elijah Smith <elijah.matthew.smith@gmail.com>
2021-01-30 03:06:02 -0500

    Full PRAW implementation, HTTP and JSON protocols

--- RetalicaBackend/Retalica/views.py
+++ RetalicaBackend/Retalica/views.py
@@ -2,53 +2,33 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 import praw
 import json
+import os
 
 # If we want to include additional subreddits
 # subreddits = ["wallstreetbets"]
 
 # Request should be incoming JSON with request for specific stock or top 5
-# Will need some way of differentiating
-# Search for a top stock
-# Top 5 on Reddit currently
+# Pick top 5 most discussed stocks on wallstreetbets currently from list
 
-# List of string stocks
-# What stocks are we looking for and what is an effective method of checking submissions for all these stocks
 def topStocks(reddit):
+    # This should be expanded as much as text editing allows
+    # However, if this becomes much larger, we need to increase efficiency here
+    NYSE = open('NYSE_Top_100.txt', 'r')
     mentionCount = {}
-    # Are there any inputs here from a JSON object? Or just JSON output?
 
-    # Demo hard coded response
-    return JsonResponse({"first": "AMZN", "second": "BKNG", "third": "GOOG", "fourth": "GOOGL", "fifth": "ISRG"})
-    # Just need the top five, not any specific information about the stock
-    # Can get a list of important stocks from somewhere to evaluate
+    line = NYSE.readline()
+    while line != '':
+        line = line.replace('\n', '').lstrip().rstrip()
+        mentionCount[line] = 0
+        line = NYSE.readline()
 
-    # TODO:
-    # How do we know what to look for? Ask the group
-
-    # .hot(), .controversial(), .new(), .rising(), .top(time_filter: str = 'all') - can be all, day, hour, month, week, year
-    #for submission in reddit.subreddit("wallstreetbets").top('hour'):
-
-
-
-
-# Outputs: Integer number of occurences
-# Yet to be decided: What subset of submissions are we taking?
-def searchStock(request): # stock is a String
-    # .hot(), .controversial(), .new(), .rising(), .top(time_filter: str = 'all') - can be all, day, hour, month, week, year
-    # Keep track of: Number of title mentions in the past hour
-    # Number of upvotes? Total number of upvotes?
-        # Probably needs to be whatever our proposed measurement is
-    
-    
-    # Wildly untested and likely to be completely wrong
-    json_data = None
-    stock = ""
-    found = 0
-
-    if request.is_ajax() and request.method == 'GET':
-        if request.method == 'GET':
-            json_data = json.loads(request.body.decode("utf-8")) # Decode might or might not be necessary
-            stock = json_data['stock']
+    '''
+    Just for evaluation
+    mentionCount['target'] = 3
+    mentionCount['Pfizer'] = 5
+    mentionCount['PepsiCo'] = 5
+    mentionCount['Nov'] = 2
+    '''
 
     reddit = praw.Reddit(
         user_agent="Retalica Stock Popularity Analysis",
@@ -56,9 +36,92 @@ def searchStock(request): # stock is a String
         client_secret="JOM9pkVc3g6aufzohc1mDDP1EOEBhw"
     )
 
-    for submission in reddit.subreddit("wallstreetbets").top('hour'):
-        if submission.title.lower().rfind(stock) == True:
+    # Needs several performance improvements here
+    for submission in reddit.subreddit("wallstreetbets").hot(limit=500):
+        for company in mentionCount:
+            currentCount = mentionCount[company]
+            if submission.title.lower().find(company) >= 0:
+                mentionCount[company] = currentCount + 1
+
+    # 6th listing is ignored, just overwrites itself 
+        # Used to prevent index going out of bounds
+    topFiveNames = ["", "", "", "", "", ""]
+    topFiveValues = [-1, -1, -1, -1, -1, -1]
+
+    for company in mentionCount:
+        count = int(mentionCount[company])
+        for i in range(0, 5):
+            # Current approach favors earlier loaded stock in ties (since ties aren't grounds for removal
+            # Future work could hold other details to break ties
+            if count > topFiveValues[i]:
+                # Push all lower elements (including i) down one, then insert at i
+                for j in range(4, i - 1, -1):
+                    topFiveValues[j + 1] = topFiveValues[j]
+                    topFiveNames[j + 1] = topFiveNames[j]
+                topFiveNames[i] = company
+                topFiveValues[i] = count
+                break
+
+    NYSE.close()
+    return JsonResponse(
+        {
+            "first": {
+                "name": topFiveNames[0],
+                "ocurrences": topFiveValues[0],
+            },
+            "second": {
+                "name": topFiveNames[1],
+                "ocurrences": topFiveValues[1],
+            },
+            "third": {
+                "name": topFiveNames[2],
+                "ocurrences": topFiveValues[2],
+            },
+            "fourth": {
+                "name": topFiveNames[3],
+                "ocurrences": topFiveValues[3],
+            },
+            "fifth": {
+                "name": topFiveNames[4],
+                "ocurrences": topFiveValues[4],
+            },
+        }
+    )
+
+def searchStock(request):
+    # Other methods of sorting
+    # .hot(), .controversial(), .new(), .rising(), .top(time_filter: str = 'all')
+    
+    json_data = None
+    # stock = ''
+    stock = 'gme' # For testing
+    found = 0
+
+    # Work on once we get Flutter-Django connection
+    # Wildly untested and likely to be completely wrong
+    '''
+    # TODO: I don't know what request.is_ajax is or does
+    # I assume we're using GET, other option being POST
+    if request.is_ajax() and request.method == 'GET':
+        if request.method == 'GET':
+            json_data = json.loads(request.body.decode("utf-8")) # Decode might or might not be necessary
+            stock = json_data['stock']
+    '''
+
+    stock = stock.lower()
+
+    reddit = praw.Reddit(
+        user_agent="Retalica Stock Popularity Analysis",
+        client_id="oylqrww5RDa-RQ",
+        client_secret="JOM9pkVc3g6aufzohc1mDDP1EOEBhw"
+    )
+
+    for submission in reddit.subreddit("wallstreetbets").hot(limit=500):
+        if submission.title.lower().find(stock) != -1:
             found += 1
 
-    # Demo hard coded response (CONFIRMED THESE JSON RESPONSES WORK, IF THAT'S GOOD ENOUGH FOR FLUTTER)
-    return JsonResponse({"submissionCount": found})
+    return JsonResponse(
+        {
+            "submissionCount": found
+        }
+    )
--- RetalicaBackend/RetalicaBackend/urls.py
+++ RetalicaBackend/RetalicaBackend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('Retalica/', include('Retalica.urls')),
+    path('', include('Retalica.urls')),
     path('admin/', admin.site.urls),
 ]
