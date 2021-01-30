a08a9ca9fd74bb3abd5d225e7a11be679a9e6081
Elijah Smith <elijah.matthew.smith@gmail.com>
2021-01-29 23:06:06 -0500

    compiling

--- RetalicaBackend/Retalica/views.py
+++ RetalicaBackend/Retalica/views.py
@@ -17,8 +17,10 @@ def index(request):
         client_secret="JOM9pkVc3g6aufzohc1mDDP1EOEBhw"
     )
 
-    return findTop(reddit)
-    #return findSpecific(reddit, stock) - String format
+    return HttpResponse("Test")
+    # return findTop(reddit)
+    # return findSpecific(reddit, stock) - String format
+
 
 def findTop(reddit):
     mentionCount = {}
@@ -30,18 +32,8 @@ def findTop(reddit):
     # How do we know what to look for? Ask the group
 
     # .hot(), .controversial(), .new(), .rising(), .top(time_filter: str = 'all') - can be all, day, hour, month, week, year
-    for submission in reddit.subreddit("wallstreetbets").top('hour'):
+    '''for submission in reddit.subreddit("wallstreetbets").top('hour'):'''
 
-    '''
-    https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
-    created_utc: Time the submission was created, represented in Unix Time.
-    num_comments: The number of comments on the submission.
-    score: The number of upvotes for the submission.
-    title: The title of the submission.
-    upvote_ratio: The percentage of upvotes from all votes on the submission.
-    '''
-
-    return HttpResponse("Trying comment API.")
 
 
 def findSpecific(reddit, stock): #stock is a String
@@ -50,30 +42,8 @@ def findSpecific(reddit, stock): #stock is a String
     # Number of upvotes? Total number of upvotes?
         # Probably needs to be whatever our proposed measurement is
     
-    for submission in reddit.subreddit("wallstreetbets").top('hour'):
-        if submission.title.lower().rfind(stock) == True:
+    '''for submission in reddit.subreddit("wallstreetbets").top('hour'):
+        if submission.title.lower().rfind(stock) == True:'''
             
 
-    '''
-    https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
-    created_utc: Time the submission was created, represented in Unix Time.
-    num_comments: The number of comments on the submission.
-    score: The number of upvotes for the submission.
-    title: The title of the submission.
-    upvote_ratio: The percentage of upvotes from all votes on the submission.
-    '''
-
 # For now, just worry about post quantity. We can evaluate other factors later.
-
-'''
-Check for what stocks are posted about the most
-Upvotes and quantity of posts weigh into most popular stocks
-
-Develop how we're weighing these stocks and return these values to Flutter, 
-which will deal with stocks API and caching values
-
-After we get ticker information, we can go back to reddit API - 
-Two main functions:
-    - JSON file that gives top five
-    - Searching for a specific stock and its information
-'''
